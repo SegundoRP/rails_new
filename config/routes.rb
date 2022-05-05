bd8873a1e08143fc21a0d12e2db103ddc19d4b67
segundo rebaza <alvaro.rp1593@gmail.com>
2022-05-05 13:53:50 -0500

    repository stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "ask", to: "questions#ask"
   # muestreme el ask y vaya al controlador questions para ver si hay alguna accion ask
+  # VERB "url", to: "controller#action"
   get "answer", to: "questions#answer"
 end
